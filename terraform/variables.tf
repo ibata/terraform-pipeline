50d463a3be21f47e2bea9ca7989d8f4c66b75a79
ibata <luc.ibata@yahoo.com>
2018-01-31 04:39:54 -0800

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,9 @@ variable "aws" {
   description = "AWS provider settings"
 
   default = {
-    access_key = ""
+    access_key = "AKIAJWXP3F57JSSFK4HA"
     profile    = "lucaws"
     region     = "us-east-1"
-    secret_key = ""
+    secret_key = "mm5zBhjdlec59MSWb4ej4fdFeMpOviRSdLu3Sd7C"
   }
 }
