b2e6cf1e951c89dcad45c2c78a8cb61a35ecc55c
ibata <luc.ibata@yahoo.com>
2018-01-31 03:46:18 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,10 @@
+# Configure the AWS Provider
+provider "aws" {
+  access_key = "AKIAJWXP3F57JSSFK4HA"
+  secret_key = "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"
+  region     = "us-east-1"
+}
+
 variable "vpc_cidr" {
   default = "10.0.0.0/16"  
 }
